eb05401645ca11a8d062ddc8bf112283f354f90a
DanyWind <36925557+DanyWind@users.noreply.github.com>
2019-02-21 16:41:06 +0100

    Added partitioning and cleaned cells

--- notebooks/Users/daniel.huynh@studentpartner.com/als-big.py
+++ notebooks/Users/daniel.huynh@studentpartner.com/als-big.py
@@ -167,16 +167,12 @@ u_rows = sc.parallelize((i,U[i]) for i in range(n))
 v_rows = sc.parallelize((i,V[i]) for i in range(m))
 
 # We create an RDD with key = (i,j) and values = rating
-y = df.rdd.map(lambda x : (usr_to_emb_broad[x["userId"]], movie_to_emb_broad[x["movieId"]], x["rating"]))
+y = df.rdd.map(lambda x : (usr_to_emb[x["userId"]], movie_to_emb[x["movieId"]], x["rating"]))
 
 # We then broadcast it
 # sc.broadcast(y.collect())
 
 for l in range(N_EPOCH):
-  
-  if l > 0:
-    u_rows = sc.parallelize(u_rows)
-    v_rows = sc.parallelize(v_rows)
   
   # First we map the observations with the local U matrix
   a = y.map(lambda x : (x[0], (x[1],x[2]))).join(u_rows)
@@ -217,8 +213,8 @@ for l in range(N_EPOCH):
   agg_grad_v = grad_v.map(lambda x : (x[0], (1,x[1]))).reduceByKey(lambda x,y : (x[0] + y[0],x[1] + y[1])).mapValues(lambda x : x[1] / x[0])
   
   # We make one step of a gradient descent
-  u_rows = u_rows.join(agg_grad_u).mapValues(lambda x : x[0] - step * x[1]).collect()
-  v_rows = v_rows.join(agg_grad_u).mapValues(lambda x : x[0] - step * x[1]).collect()
+  u_rows = u_rows.join(agg_grad_u).mapValues(lambda x : x[0] - step * x[1])
+  v_rows = v_rows.join(agg_grad_u).mapValues(lambda x : x[0] - step * x[1])
   
   #u_rows.coalesce(N_WORKERS)
   #v_rows.coalesce(N_WORKERS)
@@ -230,9 +226,15 @@ for l in range(N_EPOCH):
 
 # COMMAND ----------
 
-Time ; 8.59
+Time ; 8.59 m
 Epochs : 3
 Loss : 9.12
+
+# COMMAND ----------
+
+Time : 14.26 m
+Epochs : 5
+Loss : 9.05
 
 # COMMAND ----------
 
@@ -314,6 +316,12 @@ for l in range(N_EPOCH):
   #u_v_vect.unpersist()
   #diff.unpersist()
   #grads.unpersist()
+
+# COMMAND ----------
+
+Time : 15.83
+Epochs : 5
+Loss : 9.04
 
 # COMMAND ----------
 
@@ -407,146 +415,64 @@ for l in range(N_EPOCH):
 
 # COMMAND ----------
 
+"""Version 4 : added broadcasting"""
+
+import numpy as np
+
+# HHYPER-PARAMETERS
+N_WORKERS = 8
+N_EPOCH = 3
+step = 0.01
+hidden_size = 5
+
+n = len(usr_to_emb)
+m = len(movie_to_emb)
+
+# Matrixes which represent our embeddings
+U = np.random.randn(n,hidden_size)
+V = np.random.randn(m,hidden_size)
+
+# We then create our RDDs from the matrixes
+u_rows = sc.parallelize((i,U[i]) for i in range(n))
+v_rows = sc.parallelize((i,V[i]) for i in range(m))
+
+# First we do the mapping between the raw indexes to the normalized indexes
+y = df.rdd.map(lambda x : (usr_to_emb[x["userId"]], (movie_to_emb[x["movieId"]], x["rating"])))
+
+# Then we group by the key i of users and broadcast the dictionary (i, [(j,yij), ... ])
+y_broadcast = y.groupByKey().collectAsMap()
+y_broadcast = sc.broadcast(y_broadcast)
+
 y = df.rdd.map(lambda x : ( (x["userId"],x["movieId"]), int(x["rating"])) )
 keys = y.keys()
 keys = keys.map(lambda x : (x[0],1))
 keys = keys.reduceByKey(lambda x,y : x + y)
 
 df_test = spark.createDataFrame(keys).toDF("userId", "count")
-df_test.createTempView("test3")
+df_test.createTempView("test")
 
 # Create a RDD with cumulative count of total movies
 usr_to_count = sqlContext.sql("select userId,count, " +
   "SUM(count) over (  order by userId  rows between unbounded preceding and current row ) cum_count " +
-  "from test3")
+  "from test")
 
 usr_cum_count = usr_to_count.rdd.map(lambda x : x["cum_count"]).collect()
+
+N = df.count()
 
 def count_partition(key):
   key = int(( float(usr_cum_count[key]) / N) * N_WORKERS)
   return(key)
 
-# COMMAND ----------
-
-"""Version 3 : partition by key"""
-
-import numpy as np
-
-# HHYPER-PARAMETERS
-N_WORKERS = 8
-N_EPOCH = 25
-step = 0.01
-hidden_size = 5
-
-n = len(usr_to_emb)
-m = len(movie_to_emb)
-
-# Matrixes which represent our embeddings
-U = np.random.randn(n,hidden_size)
-V = np.random.randn(m,hidden_size)
-N = df.count()
-
-# We then create our RDDs from the matrixes
-u_rows = sc.parallelize((i,U[i]) for i in range(n))
-v_rows = sc.parallelize((i,V[i]) for i in range(m))
-
-# We create an RDD with key = (i,j) and values = rating
-y = df.rdd.map(lambda x : ( (x["userId"],x["movieId"]), int(x["rating"])) )
-
-# We then broadcast it
-sc.broadcast(y.collect())
-
 for l in range(N_EPOCH):
+  # Partitioning
+  u_rows = u_rows.partitionBy(N_WORKERS,count_partition)
   
-  if l > 0:
-    u_rows = sc.parallelize(u_rows)
-    v_rows = sc.parallelize(v_rows)
+  # First we do a lookup with the broadcast y  
+  a = u_rows.map(lambda x : ((x[0],x[1]), y_broadcast.value[x[0]]))
   
-  u_rows.partitionBy(N_WORKERS,count_partition)
-  
-  # First we map the observations with the local U matrix
-  a = y.map(lambda x : (x[0][0], (x[0][1] , x[1]) )).join(u_rows)
-  
-  # Then we reorder in order to join with the V matrix
-  b = a.map(lambda x : (x[1][0][0], (x[0], x[1][1], x[1][0][1]) ))
-  
-  # We then join and reorder into ((i,j) ,(u,v,y,y_pred,diff))
-  c = b.join(v_rows)
-  u_v_y_dot = c.map(lambda x : ((x[1][0][0], x[0]) , (x[1][0][1], x[1][1], x[1][0][2], np.dot(x[1][0][1], x[1][1]), np.dot(x[1][0][1], x[1][1]) - x[1][0][2]) ))
-
-  # Then we get a key value with keys = (i,j), and values = (u,v) i.e. the index of user/product and the associated vectors
-  #u_v_vect = u_v.map(lambda x : ( (x[0][0],x[1][0]) , (x[0][1],x[1][1]) ) ) 
-  #u_v_vect.persist()
-
-  #u_v_dot =  u_v.map(lambda x : ( (x[0][0],x[1][0]) , np.dot(x[0][1],x[1][1]) ) )
-  
-  # We then take the difference between the prediction and the real value
-  #diff = u_v_y_dot.mapValues(lambda x : (x[3] - x[2]))
-  #diff.persist()
-
-  # We compute the mean squared loss
-  loss = u_v_y_dot.mapValues(lambda x : x[4]**2).map(lambda x: x[1]).mean() / 2
-  
-  # We print the loss
-  print("Current loss at epoch %i : %f" % (l,loss))
-  
-  # We compute the gradients for each pair
-  #grads = u_v_vect.join(diff)
-  #grads.persist()
-
-  # We then compute the gradients wrt u and v
-  grad_u = u_v_y_dot.map(lambda x : (x[0][0], x[1][4] * x[1][1]))
-  grad_v = u_v_y_dot.map(lambda x : (x[0][1], x[1][4] * x[1][0]))
-  
-  # We take the average of each batch
-  agg_grad_u = grad_u.map(lambda x : (x[0], (1,x[1]))).reduceByKey(lambda x,y : (x[0] + y[0],x[1] + y[1])).mapValues(lambda x : x[1] / x[0])
-  agg_grad_v = grad_v.map(lambda x : (x[0], (1,x[1]))).reduceByKey(lambda x,y : (x[0] + y[0],x[1] + y[1])).mapValues(lambda x : x[1] / x[0])
-  
-  # We make one step of a gradient descent
-  u_rows = u_rows.join(agg_grad_u).mapValues(lambda x : x[0] - step * x[1]).collect()
-  v_rows = v_rows.join(agg_grad_u).mapValues(lambda x : x[0] - step * x[1]).collect()
-  
-  # Free u_v_vect and diff
-  #u_v_vect.unpersist()
-  #diff.unpersist()
-  #grads.unpersist()
-
-# COMMAND ----------
-
-"""Version 2 : deleted cartesian product and optimized join"""
-
-import numpy as np
-
-# HHYPER-PARAMETERS
-N_WORKERS = 8
-N_EPOCH = 100
-step = 0.01
-hidden_size = 5
-
-n = len(usr_to_emb)
-m = len(movie_to_emb)
-
-# Matrixes which represent our embeddings
-U = np.random.randn(n,hidden_size)
-V = np.random.randn(m,hidden_size)
-
-# We then create our RDDs from the matrixes
-u_rows = sc.parallelize((i,U[i]) for i in range(n))
-v_rows = sc.parallelize((i,V[i]) for i in range(m))
-
-# We create an RDD with key = (i,j) and values = rating
-y = df.rdd.map(lambda x : ( (x["userId"],x["movieId"]), int(x["rating"])) )
-
-# We then broadcast it
-sc.broadcast(y.collect())
-
-for l in range(N_EPOCH):
-  
-  # First we map the observations with the local U matrix
-  a = y.map(lambda x : (x[0][0], (x[0][1] , x[1]) )).join(u_rows)
-  
-  # Then we reorder in order to join with the V matrix
-  b = a.map(lambda x : (x[1][0][0], (x[0], x[1][1], x[1][0][1]) ))
+  # We then flatMap values of the elements x : ((i,u), [(j,y)]) and then we reorder it to join on the j 
+  b = a.flatMapValues(lambda x : x).map(lambda x : ( x[1][0], (x[0][0], x[0][1], x[1][1])))
   
   # We then join and reorder into ((i,j) ,(u,v,y,y_pred,diff))
   c = b.join(v_rows)
@@ -591,102 +517,3 @@ for l in range(N_EPOCH):
   #u_v_vect.unpersist()
   #diff.unpersist()
   #grads.unpersist()
-
-# COMMAND ----------
-
-import numpy as np
-
-# HHYPER-PARAMETERS
-
-N_EPOCH = 100
-step = 0.01
-hidden_size = 5
-
-n = len(usr_to_emb)
-m = len(movie_to_emb)
-
-# Matrixes which represent our embeddings
-U = np.random.randn(n,hidden_size)
-V = np.random.randn(m,hidden_size)
-
-# We then create our RDDs from the matrixes
-u_rows = sc.parallelize((i,U[i]) for i in range(n))
-v_rows = sc.parallelize((i,V[i]) for i in range(m))
-
-v_rows_broad = sc.broadcast(v_rows.collect())
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-""" coalesce : merger localement
- broadcaster 
- unperist
- 
- Checkpoint : toutes les 50 opérations
- Cocoa : API Spark pour updater localement
- 
- """
-
-# COMMAND ----------
-
-print("The current loss is %f" % loss)
-
-# COMMAND ----------
-
-y.count()
-
-# COMMAND ----------
-
-len(y.keys().collect())
-
-# COMMAND ----------
-
-n
-
-# COMMAND ----------
-
-n*m
-
-# COMMAND ----------
-
-len(u_v_dot.keys().collect())
-
-# COMMAND ----------
-
-372100 - 100836
-
-# COMMAND ----------
-
-u_v_y_dot.count()
-
-# COMMAND ----------
-
-diff.take(1)
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-diff
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-step = 0.01
-
-
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
